b0345a6472f65b5de8847ecd0f7ad99a4b10df26
Robert Fletcher <lobatifricha@gmail.com>
2012-10-27 12:40:36 -0700

    Initial commit!

---
+++ blocks.rb
@@ -0,0 +1,8 @@
+names.each { |name| name + "!" }
+
+# What does the above expression return?
+# Break the expression up across multiple lines.
+
+# more:
+names.collect { |name| name + "!" }
+names.map { |name| name + "!" }
---
+++ chaining.rb
@@ -0,0 +1,30 @@
+names.join
+
+# What kind of thing is the variable 'names'?
+# What kind of thing does the expression return?
+
+# more:
+names.join(" ")
+flowers.join(" ")
+flowers.join(";")
+
+##########
+
+flowers.split
+
+# What kind (Class) of thing is the variable 'flowers'?
+# What kind (Class) of thing does this expression return?
+
+# more:
+flowers.split(";")
+
+##########
+
+flowers.split.join("\n")
+
+# What kind of thing (Class) is 'flowers'?
+# What kind of thing does this expression return?
+# Break this expression down across multiple lines.  You'll need to use another variable.
+
+# more:
+words.split.join(";")
